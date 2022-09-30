925cc6e1158a7e403991a26822ae450e1948d8ac
alirex <alirex.prime@gmail.com>
2022-10-01 02:46:45 +0300

    ✨ Add webargs

--- apps/humans/views.py
+++ apps/humans/views.py
@@ -1,15 +1,20 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from webargs import fields
+from webargs.djangoparser import use_args
 
 from apps.humans.services import generate_humans
 
 
-def get_humans(request: HttpRequest) -> HttpResponse:
+@use_args({"amount": fields.Int(missing=10)}, location="query")
+def get_humans(request: HttpRequest, args) -> HttpResponse:
+    amount = args["amount"]
+
     return render(
         request,
         "humans/index.html",
         {
-            "humans": generate_humans(amount=10),
+            "humans": generate_humans(amount=amount),
             "title": "Humans",
         },
     )
