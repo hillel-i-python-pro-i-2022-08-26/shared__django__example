0915de1c182d0ad925e910b114890717f75c9ce7
alirex <alirex.prime@gmail.com>
2022-09-30 21:34:55 +0300

    ✨ Add templates

--- apps/base/views.py
+++ apps/base/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
 
 from apps.base.services.get_random_message import get_random_message
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse(get_random_message())
+    return render(
+        request,
+        "index.html",
+        {
+            "random_message": get_random_message(),
+        },
+    )
--- apps/password_generator/urls.py
+++ apps/password_generator/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("<int:password_length>", views.generate_password_view, name="index")]
+app_name = "password_generator"
+
+urlpatterns = [
+    path("<int:password_length>", views.generate_password_view, name="index"),
+]
