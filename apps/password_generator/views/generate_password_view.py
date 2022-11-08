d7296e3ecd858b0b2f5815c5c6bfd4ee69e69cfd
alirex <alirex.prime@gmail.com>
2022-11-08 21:29:30 +0200

    ✨ Add login_required and template_view

--- apps/password_generator/views/generate_password_view.py
+++ apps/password_generator/views/generate_password_view.py
@@ -1,14 +1,17 @@
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic import TemplateView
 
 from apps.password_generator.services.generate_password import generate_password
 
 
-def generate_password_view(request: HttpRequest, password_length: int) -> HttpResponse:
-    return render(
-        request,
-        "password_generator/index.html",
-        {
-            "password": generate_password(password_length=password_length),
-        },
-    )
+@method_decorator(login_required, name="get")
+class GeneratePasswordView(TemplateView):
+    template_name = "password_generator/index.html"
+
+    def get_context_data(self, password_length: int, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+
+        context_data["password"] = generate_password(password_length=password_length)
+
+        return context_data
