ed8eb8664fc2188c09260b13e3ac18ab857ae527
alirex <alirex.prime@gmail.com>
2022-11-11 22:59:28 +0200

    ✨ Improve Animals. Rework forms and templates.

--- apps/animals/views.py
+++ apps/animals/views.py
@@ -1,12 +1,24 @@
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
-from django.views.generic import UpdateView, ListView
+from django.views.generic import UpdateView, ListView, CreateView
 
 from .models import Animal
 
 
 class ArticleListView(ListView):
     model = Animal
+
+
+class AnimalCreateView(CreateView):
+    model = Animal
+    fields = (
+        "name",
+        "age",
+        "avatar",
+    )
+
+    success_url = reverse_lazy("animals:index")
 
 
 @method_decorator(login_required, name="post")
@@ -18,4 +30,5 @@ class AnimalUpdateView(UpdateView):
         "age",
         "avatar",
     )
-    template_name_suffix = "_update_form"
+
+    success_url = reverse_lazy("animals:index")
